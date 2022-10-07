9d0929aaa906b00c63b207a777d502702cb6c33e
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2022-10-07 19:00:44 +0200

    implemented json object convertion before storing date

--- file_writer.py
+++ file_writer.py
@@ -1,3 +1,5 @@
+import json
+
 def create_pwd_file(filename, content):
     file = open(filename, "w+")
     file.write(content)
@@ -9,8 +11,20 @@ def create_new_pwd(filename, content):
     file.close()
 
 def create_new_entry(filename, username, email, pwd, comment, url, notes):
-    content = "\n" + "{" + "\n \t\t"  + username + ",\n \t\t" + ",\n \t\t" + email + ",\n \t\t"+ pwd + ",\n \t\t" + comment + ",\n \t\t" + url + ",\n \t\t" + ",\n \t\t" + notes + "\n"+"}" + "\n"
+    #x = "\n" + "{" + "\n \t\t"  + username + ",\n \t\t" + ",\n \t\t" + email + ",\n \t\t"+ pwd + ",\n \t\t" + comment + ",\n \t\t" + url + ",\n \t\t" + ",\n \t\t" + notes + "\n"+"}" + "\n"
 
+    content = {
+        "username" : username,
+        "email" : email,
+        "pwd" : pwd,
+        "comment" : comment,
+        "url" : url,
+        "note" : notes
+    }
+
+    json_content = json.dumps(content)
+    print(json_content)
     file = open(filename, "a+")
-    file.write(content)
+    file.write("\n\n")
+    file.write(json_content)
     file.close()
--- main.py
+++ main.py
@@ -7,3 +7,4 @@ pwdGenerator.passwordGenerator(30)
 file_writer.create_pwd_file("test.txt", "--------------------- Passwords ---------------------")
 
 file_writer.create_new_entry("test.txt", "magno", "magno.maximo@test.ch", pwdGenerator.passwordGenerator(20), "test", "", "")
+file_writer.create_new_entry("test.txt", "tester", "tester.testing@test.ch", pwdGenerator.passwordGenerator(20), "hi", "", "")
